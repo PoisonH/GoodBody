31e0807591219816166ab20925303a8cac08c342
PoisonH <PoisonH@163.com>
2016-02-17 22:16:13 +0800

    添加代码

--- app/src/main/java/com/poison/goodbody/MainActivity.java
+++ app/src/main/java/com/poison/goodbody/MainActivity.java
@@ -2,14 +2,33 @@ package com.poison.goodbody;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity
 {
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+        initToolbar();
+    }
+
+    private void initView()
+    {
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+    }
+
+
+    private void initToolbar()
+    {
+        mToolbar.setTitle("好身体");
+        setSupportActionBar(mToolbar);
+        //给左上角加上一个返回的图标
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
